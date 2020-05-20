56253c9c7f743ce2b3c90f3a2d9b7bb021271934
calvinreu <j.calvin.reu@protonmail.com>
2020-05-20 20:25:25 +0200

    removed old code

--- population.cpp
+++ population.cpp
@@ -6,35 +6,6 @@ NEAT::population::population(const size_t &populationSize, const size_t &parentC
     parentNNets(parentC),
     parentGenes(parentC)
 {}
-/*
-const vector<NEAT::NNetInfo>& NEAT::population::getParents(const NEAT::vector<size_t> &parentI)
-{
-    if (parentI.size() != parents.size())
-        throw("different vector size: population::getParents");
-
-    NEAT::NNetInfo currentParent;
-    vector<nnet::neural_network> parentNNet(parents.size());
-
-    for (size_t i = 0; i < parents.size(); i++)
-    {
-        currentParent.NNET = NNets[parentI[i]].get_info();
-        //currentParent.NNetGenes = NNetGenes[parentI[i]];
-        parents[parentI[i]] = currentParent;
-        parentNNet.push(std::move(parentI[i]));
-    }
-    
-    NNets.free(NNets.size());
-    //NNetGenes.free(NNetGenes.size());
-
-    for (size_t i = 0; i < parents.size(); i++)
-    {
-        NNets.push(std::move(parentNNet[i]));
-        //NNetGenes.push<const genes&>(parents[i].NNetGenes);
-    }
-    
-    return parents;
-}
-*/
 
 void NEAT::population::setMutation()
 {
